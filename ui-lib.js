2357d27c11ba9caf4985d6a2b9efe6b993b09f12
Lior Shefer <lior.shefer@wix.com>
2014-10-24 12:59:47 +0300

    Upgrade to version 2.1.8

--- ui-lib.js
+++ ui-lib.js
@@ -5069,3 +5069,41 @@ jQuery.fn.definePlugin('Tooltip', function ($) {
 	};
 });
 
+
+jQuery.fn.definePlugin('Upgrade', function ($) {
+    'use strict';
+
+    return {
+        init: function () {
+            this.markup();
+            this.bindEvents();
+        },
+        getDefaults: function () {
+            return {
+                class: 'btn-upgrade',
+                vendorProductId: '',
+                cycle: 'MONTHLY'
+            };
+        },
+        markup: function () {
+            var className = "submit uilib-btn " + this.options.class;
+            this.$el.append('<button class="' + className + '">' + this.options.text + '</button>');
+        },
+        bindEvents: function () {
+            this.$el.find('.uilib-btn').on('click', this.upgrade.bind(this));
+        },
+        getValue: function () {
+        },
+        setValue: function (value) {
+        },
+        upgrade: function () {
+            var vendorProductId = this.options.vendorProductId;
+            Wix.Billing.openBillingPageForProduct(vendorProductId, this.options.cycle, function () {
+                var popup = Wix.UI.create({ctrl: 'Popup',
+                    options: {buttonSet: 'okCancel', fixed: true, title: 'Ooops something when wrong...', content: vendorProductId + ' was not found.'}});
+                popup.getCtrl().open();
+            });
+        }
+    };
+
+});
