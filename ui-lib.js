a8cb91f70379906d13d0af6424771cf63f117bee
yaarams <yaarac@wix.com>
2015-05-20 11:43:52 +0300

    Update to version 2.1.12

--- ui-lib.js
+++ ui-lib.js
@@ -2960,7 +2960,7 @@ jQuery.fn.definePlugin('FontPicker', function () {
         var fontFamilyArr = cssFontFamily.split(',');
 
         for (var fontFamily in fontFamilyArr) {
-            if (fontFamilyArr[fontFamily].indexOf('"') < 0) {
+            if (fontFamilyArr[fontFamily].indexOf('"') < 0 && fontFamilyArr[fontFamily].indexOf("'") < 0) {
                 fontFamilyArr[fontFamily]  = '"' + fontFamilyArr[fontFamily]  + '"';
             }
         }
@@ -3574,87 +3574,88 @@ jQuery.fn.definePlugin('Input', function ($) {
 });
 
 jQuery.fn.definePlugin('LanguagePicker', function () {
-    'use strict';
+	'use strict';
 
-    var styles = {
-        className: 'uilib-languagePicker'
-    };
+	var styles = {
+		className: 'uilib-languagePicker'
+	};
 
-    var symbToName = {
-        'En': 'English',
-        'De': 'Deutsch',
-        'Es': 'Español',
-        'Fr': 'Français',
-        'It': 'Italiano',
-        'Pl': 'Polski',
-        'Pt': 'Português',
-        'Ru': 'Русский',
-        'Ja': '日本語',
-        'Ko': '한국어',
-        'Tr': 'Türkçe',
-        'Nl': 'Nederlands'
-    };
+	var symbToName = {
+		'En': 'English',
+		'De': 'Deutsch',
+		'Es': 'Español',
+		'Fr': 'Français',
+		'It': 'Italiano',
+		'Pl': 'Polski',
+		'Pt': 'Português',
+		'Ru': 'Русский',
+		'Ja': '日本語',
+		'Ko': '한국어',
+		'Tr': 'Türkçe',
+		'Nl': 'Nederlands',
+		'He': 'עברית'
+	};
 
-    return {
-        init: function () {
-            this.markup();
-            if (symbToName[this.options.selectedLanguage]) {
-                this.setValue(this.options.selectedLanguage);
-            }
-            this.bindEvents();
-        },
-        markup: function () {
-            var $options = _optionsHtml(this.options.languages);
-            this.$el.append($options);
-            var height = this.options.height;
+	return {
+		init: function () {
+			this.markup();
+			if (symbToName[this.options.selectedLanguage]) {
+				this.setValue(this.options.selectedLanguage);
+			}
+			this.bindEvents();
+		},
+		markup: function () {
+			var $options = _optionsHtml(this.options.languages);
+			this.$el.append($options);
+			var height = this.options.height;
 
-            if (this.options.height === 'auto') {
-                var m = $options.length % 2;
-                height = $options.length <= 4 ? $options.length * 26 : ($options.length - m) / 2 * 26;
-            }
+			if (this.options.height === 'auto') {
+				var m = $options.length % 2;
+				height = $options.length <= 4 ? $options.length * 26 : ($options.length - m) / 2 * 26;
+			}
 
-            this.dropdown = this.$el.Dropdown({
-                width: 62,
-                height: height,
-                optionsWidth: 105,
-                modifier: function ($el) {
-                    var $globe = $("<span class='globe'></span>");
-                    $el.text($el.attr('data-value'));
-                    $el.prepend($globe);
-                    return $el;
-                }
-            }).data('plugin_Dropdown');
+			this.dropdown = this.$el.Dropdown({
+				width: 62,
+				height: height,
+				optionsWidth: 105,
+				modifier: function ($el) {
+					var $globe = $("<span class='globe'></span>");
+					$el.text($el.attr('data-value'));
+					$el.prepend($globe);
+					return $el;
+				}
+			}).data('plugin_Dropdown');
 
-            if (!this.$el.hasClass(styles.className)) {
-                this.$el.addClass(styles.className);
-            }
-        },
-        bindEvents: function () {
-            var languagePicker = this;
-            this.$el.on('Dropdown.change', function (evt, data) {
-                evt.stopPropagation();
-                languagePicker.triggerChangeEvent(languagePicker.getValue());
-            });
-        },
-        getDefaults: function () {
-            return {
-                languages: ['En', 'De', 'Es', 'Fr', 'It', 'Pl', 'Pt', 'Ru', 'Ja', 'Ko', 'Tr', 'Nl'],
-                height: 'auto'
-            };
-        },
-        getValue: function () {
-            return this.dropdown.getFullValue();
-        },
-        setValue: function (value) {
-            return this.dropdown.setValue(value);
-        }
-    };
+			if (!this.$el.hasClass(styles.className)) {
+				this.$el.addClass(styles.className);
+			}
+		},
+		bindEvents: function () {
+			var languagePicker = this;
+			this.$el.on('Dropdown.change', function (evt, data) {
+				evt.stopPropagation();
+				languagePicker.triggerChangeEvent(languagePicker.getValue());
+			});
+		},
+		getDefaults: function () {
+			return {
+				languages: ['En', 'De', 'Es', 'Fr', 'It', 'Pl', 'Pt', 'Ru', 'Ja', 'Ko', 'Tr', 'Nl', 'He'],
+				height: 'auto'
+			};
+		},
+		getValue: function () {
+			return this.dropdown.getFullValue();
+		},
+		setValue: function (value) {
+			return this.dropdown.setValue(value);
+		}
+	};
 
-    function _optionsHtml(langs) {
-        return $(langs.map(function (symb) {
-            return '<div value="' + symb + '">' + symbToName[symb] + '</div>';
-        }).join(''));
-    }
+	function _optionsHtml(langs) {
+		return $(langs.map(function (symb) {
+			return '<div value="' + symb + '">' + symbToName[symb] + '</div>';
+		}).join(''));
+	}
 });
 
 jQuery.fn.definePlugin('Popup', function ($) {
@@ -4609,7 +4610,10 @@ jQuery.fn.definePlugin('Slider', function ($) {
 			}
 		},
 		getXFromEvent: function(event){
-			return event.offsetX / this.$el.width()
+			var x = event.offsetX !== undefined
+					? event.offsetX
+					: event.clientX - event.target.getBoundingClientRect().left;
+			return x / this.$el.width();
 		},
 		bindEvents: function () {
 			var $body = $(window);
@@ -4828,9 +4832,11 @@ jQuery.fn.definePlugin('Spinner', function ($) {
 		},
 		setValue: function (valueInRange) {
             this.options.value = this.valueInRangeToInnerRange(valueInRange);
+			if (valueInRange !== this.last_value) {
+				this.update();
+			}
 			if (this.options.value !== this.last_value) {
                 this.last_value = this.options.value;
-				this.update();
 				return true;
 			}
 		},
